a8cd0c704056fb44ba4fc53a876c73f18330d5fe
DarioCabas <hz-hertzio@hotmail.com>
2023-10-19 14:59:37 -0500

    modify dialog and add icon

--- src/components/GalleryReact.tsx
+++ src/components/GalleryReact.tsx
@@ -40,9 +40,18 @@ const GalleryReact = () => {
             <div key={index} className="flex w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-1 md:p-2 lg:h-[400px]">
 
               <div className="relative " onClick={handleDialogOpen} >
-                <img className="block mx-auto h-full object-cover object-center rounded-lg shadow-md" src={image}  key={index} />
+                <img className="block mx-auto h-full object-cover object-center rounded-lg shadow-md transition duration-200 hover:scale-105" src={image} key={index} />
                 <div className="absolute bottom-0 left-0 p-2 bg-transparent text-white">
                   Description for the image
+                </div>
+                <div className="absolute bottom-2 right-2 cursor-pointer" onClick={handleDialogOpen}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-maximize text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M4 8v-2a2 2 0 0 1 2 -2h2"></path>
+                    <path d="M4 16v2a2 2 0 0 0 2 2h2"></path>
+                    <path d="M16 4h2a2 2 0 0 1 2 2v2"></path>
+                    <path d="M16 20h2a2 2 0 0 0 2 -2v-2"></path>
+                  </svg>
                 </div>
               </div>
 
