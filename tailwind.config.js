94d837e66587fc7b14264c67b6eb2f482283b5c5
DarioCabas <hz-hertzio@hotmail.com>
2023-11-10 18:00:56 -0500

    add next ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	"./node_modules/tw-elements/dist/js/**/*.js"],
+	"./node_modules/tw-elements/dist/js/**/*.js",
+	"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+],
 	theme: {
 		extend: {},
 	},
-	plugins: [require("tw-elements/dist/plugin.cjs")],
+	plugins: [require("tw-elements/dist/plugin.cjs"), nextui()],
 }
